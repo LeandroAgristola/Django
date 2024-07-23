7132d2a286f82a4829744bc524d04b987d24b305
LeandroAgristola <leandro.agristola@gmail.com>
2024-07-23 20:46:12 -0300

    cambios videos 34

--- Proyectoweb/servicios/models.py
+++ Proyectoweb/servicios/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 
-class Sevicio(models.Model):
+class Servicio(models.Model):
     titulo=models.CharField(max_length=50)
     contenido=models.CharField(max_length=50)
-    imagen=models.ImageField
+    imagen=models.ImageField()
     created=models.DateTimeField(auto_now_add=True) #se utiliza para filtrar por fecha de creacion 
     updated=models.DateTimeField(auto_now_add=True) #se utiliza para filtrar por fecha de actualizacion 
 
