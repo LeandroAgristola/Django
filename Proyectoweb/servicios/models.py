d5d82b0781c6aba50dd00c321afe97fea3623372
LeandroAgristola <leandro.agristola@gmail.com>
2024-07-23 20:06:02 -0300

    cambios video 33

--- Proyectoweb/servicios/models.py
+++ Proyectoweb/servicios/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Sevicio(models.Model):
+    titulo=models.CharField(max_length=50)
+    contenido=models.CharField(max_length=50)
+    imagen=models.ImageField
+    created=models.DateTimeField(auto_now_add=True) #se utiliza para filtrar por fecha de creacion 
+    updated=models.DateTimeField(auto_now_add=True) #se utiliza para filtrar por fecha de actualizacion 
+
+    class Meta:
+        verbose_name='servicio' # Esta opción define el nombre descriptivo singular que se utilizará para mostrar una instancia individual de tu modelo en el panel de administración. En este caso, se establece como "servicio".
+        verbose_name_plural='servicios' #Esta opción define el nombre descriptivo plural que se utilizará para mostrar múltiples instancias de tu modelo en el panel de administración. En este caso, se establece como "servicios"
+
+    def __str__(self):
+        return self.titulo
