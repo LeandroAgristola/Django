a8fff942144be29ca17b4efb14284fb9e599e164
leopoldazzo <leopoldaso007@gmail.com>
2024-07-31 15:10:52 +0000

    cambios video 36

--- Proyectoweb/ProyectowebApp/views.py
+++ Proyectoweb/ProyectowebApp/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import render, HttpResponse
-from servicios.models import Servicio #importamos el servicio del models de la APP servicios para poder trabajar sobre la vista servicios
 
 # Create your views here.
 
 def home(request):
 
     return render(request,"ProyectowebApp/home.html")
-
-def servicios(request):
-
-    servicios=Servicio.objects.all()# le deciamos a django que importe todos los servicios que creamos 
-    return render(request,"ProyectowebApp/servicios.html",{"servicios":servicios})
 
 def tienda(request):
 
