8976cde87d9844c3027acf1d05e48a468954a40c
leopoldazzo <leopoldaso007@gmail.com>
2024-08-02 16:18:00 +0000

    cambios videos 42

--- Proyectoweb/blog/views.py
+++ Proyectoweb/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from blog.models import Post #importamos el servicio del models de la APP servicios para poder trabajar sobre la vista servicios
+from blog.models import Post, Categoria #importamos el servicio del models de la APP servicios para poder trabajar sobre la vista servicios
 
 # Create your views here.
 
@@ -7,3 +7,10 @@ from blog.models import Post #importamos el servicio del models de la APP servic
 def blog(request):
     posts=Post.objects.all()
     return render(request,"blog/blog.html", {"posts": posts})
+
+#Creamos la funcion que vamos a utilizar para filtrar los post por categoria, devuelvo el id correspondiente a cada categoria
+def categoria(request, categoria_id): 
+    categoria=Categoria.objects.get(id=categoria_id)
+    posts=Post.objects.filter(categorias=categoria)
+    #en el render nos tiene que motrar la categoria filtrada y luego los post de esa categoria filtradad
+    return render(request,"blog/categoria.html", {'categoria': categoria,"posts": posts})
