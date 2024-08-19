9ce1cc22e86604189502624a9216c339af10be0f
leopoldazzo <leopoldaso007@gmail.com>
2024-08-19 17:38:32 +0000

    formulario de login y aut_ video 64

--- Proyectoweb/autenticacion/urls.py
+++ Proyectoweb/autenticacion/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import VRegistro, cerrar_sesion
+from .views import VRegistro, cerrar_sesion, logear
 
 urlpatterns = [
     path('',VRegistro.as_view(), name="Autenticacion"), #nos muestra la clase VRegistro como un vista
-    path('cerrar_sesion',cerrar_sesion.as_view(), name="cerrar_sesion"), #nos muestra la clase VRegistro como un vista
+    path('cerrar_sesion',cerrar_sesion.as_view(), name="cerrar_sesion"), #nos muestra la clase cerrar sesion como un vista
+    path('logear',logear.as_view(), name="logear"), #nos muestra la clase VRegistro como un vista
 ]
+
 
--- Proyectoweb/autenticacion/views.py
+++ Proyectoweb/autenticacion/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render,redirect
 from django.views.generic import View
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
 
 # Create your views here.
 
@@ -24,6 +25,25 @@ class VRegistro(View):
             return render(request,"registro/registro.html",{"form":form}) #mostramos el formulario con los errores 
         
 
-def cerrar_sesion(request): #creamola funcion para cerrar sesion
+def cerrar_sesion(request): #creamos la funcion para cerrar sesion
     logout(request)
     return redirect('Home') #una vez cerrada la sesion, redireccionamos al home
+
+def logear(request): #creamos la funcion para iniciar sesion
+    if request.method=="POST": #si el usuario apreto en el boton de Login
+        form=AuthenticationForm(request,date=request.POST)#guardamos en la variable form, los datos del formulario ingresado en login
+        if form.is_valid(): #si los datos del formulario login son correctos
+            nombre_usuario=form.cleaned_data.get("username")
+            contra=form.cleaned_data.get("password")
+            usuario=authenticate(username=nombre_usuario, password=contra)
+            if usuario is not None: # Si el usuario existe, no logea y nos redirecciona al home
+                login(request, usuario)
+                return redirect('Home')
+            else: #si el usuario no existe
+                messages.error(request,"Usuario no valido!")
+        else: #la informacion no se a introducido correctamente
+            messages.error(request,"Informacion incorrecta")
+
+    form=AuthenticationForm()
+    return render(request,"login/login.html",{"form":form})
+
