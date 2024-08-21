223f90088f03df76d4cc8829b5d2fcb78a9f19a8
leopoldazzo <leopoldaso007@gmail.com>
2024-08-21 17:13:24 +0000

    App pedidos continuacion. video 69

--- Proyectoweb/pedidos/views.py
+++ Proyectoweb/pedidos/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from carro.carro import Carro
+from pedidos.models import LineaPedidos, Pedido
+from django.contrib import messages
+
 
 # Create your views here.
+
+@login_required(login_url="/autenticacion/logear")
+def procesar_pedido(request):
+    pedido=pedido.objects.create(user=request.user)
+    carro=Carro(request)
+    lineas_pedido=list()
+    for key,  value in carro.carro.items():
+        lineas_pedido.append(LineaPedidos(
+            
+            producto_id=key,
+            cantidad=value["cantidad"],
+            user=request.user,
+            pedido=pedido,
+            
+        ))
+    
+    LineaPedidos.objects.bulk_create(lineas_pedido)
+
+    enviar_mail(
+        pedido=pedido,
+        lineas_pedido=lineas_pedido,
+        nombreusuario=request.username,
+        emailusuario=request.usermail
+    )
+
+    messages.succes(request, "El pedido se ha creado correctamente")
+
+    return redirect("../tienda")
