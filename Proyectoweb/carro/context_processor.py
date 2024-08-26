40ac8b49c7f4cdf1f1d6dc42130901cfa40b0e2b
LeandroAgristola <leandro.agristola@gmail.com>
2024-08-26 18:14:07 -0300

    .

--- Proyectoweb/carro/context_processor.py
+++ Proyectoweb/carro/context_processor.py
@@ -1,6 +1,6 @@
 def importe_total_carro(request):
     total = 0
-    # Verifica si 'carro' está en la sesión
+    # Verifica si 'carro' está en la sesión.
     if request.user.is_authenticated:
         for key, value in request.session["carro"].items():
             total=total+float(value["precio"]) 
