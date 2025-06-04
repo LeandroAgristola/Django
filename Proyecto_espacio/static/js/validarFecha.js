6197c9f062a93b94760f0b0bde94fbbd64f9ff1b
LeandroAgristola <leandro.agristola@gmail.com>
2025-06-04 19:44:34 -0300

    eliminamos carpeta de Djnago

--- Proyecto_espacio/static/js/validarFecha.js
+++
@@ -1,8 +0,0 @@
-function validarFecha(formulario, inputId) {
-    const inputFecha = document.getElementById(inputId);
-    if (!inputFecha.value) {
-        inputFecha.classList.add('is-invalid');
-        return false;
-    }
-    return true;
-}
